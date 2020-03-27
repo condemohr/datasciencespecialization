8b9d5c9f96737d52f46f414af4ee56d457e090b6
condemohr <kike2264@yahoo.com>
2020-03-27 17:55:47 +0100

    Finished writing explicit coercion

--- vectors.R
+++ vectors.R
@@ -13,3 +13,27 @@ x<-c(1+0i,2+4i)       ## complex
 #vector() function 
 #
 x<-vector("numeric", length=10)
+
+#
+#MIXING OBJECTS
+#
+y<-c(1.7,"a")        ## character
+y<-c(TRUE,2)         ## numeric
+y<-c("a",TRUE)       ## character
+
+#
+#EXPLICIT COERCION
+#
+#Objects can be explicitly coerced from one class to another using the as.* functions
+x<-0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+
+#Non essential coercion results in NA
+
+x<-c("a","b","c")
+as.numeric(x)
+as.logical(x)
+as.complex(x)
